db686326a7644c172036963391c196d4fdb0ef0d
HADEL RACHID DAHER JUNIOR <hadelrachid@gmail.com>
2026-08-18 18:14:04 -0300

    fix: call correct addRoute method in SystemAdmin Plugin

--- src/Plugins/SystemAdmin/Plugin.php
+++ src/Plugins/SystemAdmin/Plugin.php
@@ -18,24 +18,24 @@ class Plugin extends AbstractPlugin
         $dashboardController = new DashboardController($this->container);
 
         // Dashboard base
-        $router->add('GET', '/admin', function() use ($dashboardController) {
+        $router->addRoute('GET', '/admin', function() use ($dashboardController) {
             return $dashboardController->index();
         });
 
         // Rotas de Plugins
-        $router->add('GET', '/admin/plugins', function() use ($adminController) {
+        $router->addRoute('GET', '/admin/plugins', function() use ($adminController) {
             return $adminController->listPlugins();
         });
 
-        $router->add('POST', '/admin/plugins/toggle', function() use ($adminController) {
+        $router->addRoute('POST', '/admin/plugins/toggle', function() use ($adminController) {
             return $adminController->togglePlugin();
         });
 
-        $router->add('POST', '/admin/plugins/upload', function() use ($adminController) {
+        $router->addRoute('POST', '/admin/plugins/upload', function() use ($adminController) {
             return $adminController->uploadPlugin();
         });
 
-        $router->add('POST', '/admin/plugins/delete', function() use ($adminController) {
+        $router->addRoute('POST', '/admin/plugins/delete', function() use ($adminController) {
             return $adminController->deletePlugin();
         });
     }
