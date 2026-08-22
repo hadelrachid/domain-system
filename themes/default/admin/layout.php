1a1a68cd7a0f90fa63a2e9afd8b48b44489cbd5b
HADEL RACHID DAHER JUNIOR <hadelrachid@gmail.com>
2026-08-21 21:10:47 -0300

    style: substituir titulo em texto pelo novo logotipo SVG gerado para o Cockpit

--- themes/default/admin/layout.php
+++ themes/default/admin/layout.php
@@ -30,9 +30,8 @@
 </head>
 <body>
     <div id="adminmenuback">
-        <div style="padding: 20px 15px; background: #111; text-align: center; font-weight: bold; font-size: 14px; border-bottom: 1px solid #2c3338; letter-spacing: 0.5px;">
-            <span style="color: #72aee6;">Cockpit</span> <br>
-            <span style="font-size: 11px; color: #999; font-weight: normal;">do Domain System</span>
+        <div style="padding: 20px 15px; background: #111; text-align: center; border-bottom: 1px solid #2c3338;">
+            <img src="<?= BASE_URL ?>/assets/img/logo.svg" alt="Cockpit Logo" style="max-width: 130px; height: auto;">
         </div>
         <ul id="adminmenu">
             <li><a href="admin">Painel</a></li>
