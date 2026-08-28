693e6db9024e88b8e60ae9591b9cf24c4c36b937
HADEL RACHID DAHER JUNIOR <hadelrachid@gmail.com>
2026-08-27 22:08:49 -0300

    fix(core): corrige parser de .env (parse_ini_file precisa de aspas para caracteres especiais) e adiciona verificacao de array

--- bootstrap.php
+++ bootstrap.php
@@ -18,8 +18,10 @@ putenv("DB_DSN=sqlite:{$dbPath}");
 $envFile = DOMAIN_SYSTEM_ROOT . '/.env';
 if (file_exists($envFile)) {
     $envVariables = parse_ini_file($envFile);
-    foreach ($envVariables as $key => $value) {
-        putenv("$key=$value");
+    if (is_array($envVariables)) {
+        foreach ($envVariables as $key => $value) {
+            putenv("$key=$value");
+        }
     }
 }
 
