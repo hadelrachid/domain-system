ef9220e0603b4b0b618ee9c6efc882f187790b8e
HADEL RACHID DAHER JUNIOR <hadelrachid@gmail.com>
2026-09-22 23:06:33 -0300

    chore: Remove scripts de utilidade local (reinstall.php, fix_profile.php)

--- fix_profile.php
+++
@@ -1,7 +0,0 @@
-<?php
-$app = require 'bootstrap.php';
-$app->getDispatcher()->dispatch('kernel_pre_boot');
-$app->boot();
-$db = $app->getContainer()->make(\DomainSystem\Plugins\Database\Connection::class)->getPdo();
-$db->exec("ALTER TABLE users ADD COLUMN profile_image VARCHAR(255) NULL");
-echo "OK";
